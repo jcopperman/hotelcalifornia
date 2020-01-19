b5737da813f02f343c3797867090a497463eade6
jcopperman <opperman.jonathan@gmail.com>
2020-01-19 13:41:42 +0200

    Getting create test to pass

--- Tests/HotelBookingTests.cs
+++ Tests/HotelBookingTests.cs
@@ -2,43 +2,64 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace HotelBookingTests
 {
     public class Tests
     {
-        IWebDriver driver;
+        IWebDriver _driver;
 
+        public string homeUrl = "http://hotel-test.equalexperts.io/";
 
         [SetUp]
         public void Setup()
         {
-            driver = new ChromeDriver();
-            driver.Url = "http://hotel-test.equalexperts.io/";
+            _driver = new ChromeDriver();
+            
         }
-
-        [Test]
         public void NavigatetToTheHomepage()
         {
-            
+            _driver.Navigate().GoToUrl(homeUrl);
+        }
+
+        public void DeleteExistingHotelBookingRecord()
+        {
+            IWebElement element = _driver.FindElement(By.Id("firstname"));
         }
 
         [Test]
         public void Test_Case_003_CreateNewBookingRecord()
         {
+            NavigatetToTheHomepage();
+            IWebElement firstname = _driver.FindElement(By.Id("firstname"));
+            IWebElement lastname = _driver.FindElement(By.Id("lastname"));
+            IWebElement totalprice = _driver.FindElement(By.Id("totalprice"));
+            IWebElement depositpaid = _driver.FindElement(By.Id("depositpaid"));
+            IWebElement checkin = _driver.FindElement(By.Id("checkin"));
+            IWebElement checkout = _driver.FindElement(By.Id("checkout"));
+            IWebElement savebutton = _driver.FindElement(By.CssSelector("#form > div:nth-child(1) > div:nth-child(7) > input:nth-child(1)"));
+            firstname.SendKeys("Doctor");
+            lastname.SendKeys("Who");
+            totalprice.SendKeys("1000");
+            checkin.SendKeys("2020-12-25");
+            checkout.SendKeys("2020-12-25");
+            savebutton.Click();
+            Thread.Sleep(3000);
             Assert.Pass();
         }
 
         [Test]
         public void Test_Case_004_DeleteExistingBookingRecord()
         {
+            NavigatetToTheHomepage();
             Assert.Pass();
         }
 
         [TearDown]
         public void CloseBrowser()
         {
-            driver.Close();
+            _driver.Close();
         }
     }
 }
